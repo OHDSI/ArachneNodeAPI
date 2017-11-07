a44192a22ae3c3a49cec9761df1b0798cc4a0881
Alexandr Ryabokon <mr.alexandr.ryabokon@gmail.com>
2017-11-07 17:46:53 +0300

    ARACHNE-1545 Fixed merge error

--- src/main/java/com/odysseusinc/arachne/datanode/controller/BaseDataSourceController.java
+++ src/main/java/com/odysseusinc/arachne/datanode/controller/BaseDataSourceController.java
@@ -62,6 +62,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public abstract class BaseDataSourceController<DS extends DataSource, BusinessDTO extends DataSourceBusinessDTO, CommonDTO extends CommonDataSourceDTO> extends BaseController {
 
