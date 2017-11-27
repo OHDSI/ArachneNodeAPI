ed1abfad1d4be3571f039003477d8cd58217a067
Maria Pozhidaeva <maria.pozhidaeva@odysseusinc.com>
2017-11-27 22:57:25 +0300

    ARACHNE-1603

--- src/main/resources/cc/main.r
+++ src/main/resources/cc/main.r
@@ -8,4 +8,31 @@ pw <- Sys.getenv("DBMS_PASSWORD")
 cdmDatabaseSchema <- Sys.getenv("DBMS_SCHEMA")  # may be null if not defined in datasource's metadata
 resultsDatabaseSchema <- Sys.getenv("RESULT_SCHEMA")  # may be null if not defined in datasource's metadata
 
-run_cohort_characterization(file.path(workDir, "{{initialFileName}}"), file.path(workDir, "output"), dbms, connStr, user, pw, cdmDatabaseSchema, resultsDatabaseSchema, cohortTable = "cohort", includeDrilldownReports = FALSE)
+run_cohort_characterization(cohortDefinitionSqlPath = file.path(workDir, "{{initialFileName}}"),
+                                                      outputFolder = file.path(workDir, "output"),
+                                                      dbms = dbms,
+                                                      connectionString = connStr,
+                                                      user = user,
+                                                      password = pw,
+                                                      cdmDatabaseSchema = cdmDatabaseSchema,
+                                                      resultsDatabaseSchema = resultsDatabaseSchema,
+                                                      cohortTable = "cohort",
+                                                      includeDrilldownReports = FALSE,
+                                                      includedReports = list("cohortSpecific" = FALSE,
+                                                                             "death" = FALSE,
+                                                                             "cohortObservationPeriod" = FALSE,
+                                                                             "person" = FALSE,
+                                                                             "dataCompleteness" = FALSE,
+                                                                             "dashboard" = FALSE,
+                                                                             "heraclesHeel" = FALSE,
+                                                                             "entropy" = FALSE,
+                                                                             "conditionTreemap" = FALSE,
+                                                                             "drugEraTreemap" = FALSE,
+                                                                             "drugExposuresTreemap" = FALSE,
+                                                                             "procedureTreemap" = FALSE,
+                                                                             "visitTreemap" = FALSE,
+                                                                             "conditionEraTreemap" = FALSE,
+                                                                             "conditionsByIndex" = FALSE,
+                                                                             "proceduresByIndex" = FALSE,
+                                                                             "drugsByIndex" = FALSE)
+)
