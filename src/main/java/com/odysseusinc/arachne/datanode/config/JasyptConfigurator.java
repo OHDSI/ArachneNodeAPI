7d6243610a89f18a442aa105da79ac0f0143d147
Vitaly Koulakov <vitaly.koulakov@firstlinesoftware.com>
2017-12-04 13:23:22 +0300

    ARACHNE-1647 Encrypt all passwords stored in DB (e.g. DS passwords)

---
+++ src/main/java/com/odysseusinc/arachne/datanode/config/JasyptConfigurator.java
@@ -0,0 +1,54 @@
+/*
+ *
+ * Copyright 2017 Observational Health Data Sciences and Informatics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Pavel Grafkin, Alexander Saltykov, Vitaly Koulakov, Anton Gackovka, Alexandr Ryabokon, Mikhail Mironov
+ * Created: Dec 1, 2017
+ *
+ */
+
+package com.odysseusinc.arachne.datanode.config;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.hibernate4.encryptor.HibernatePBEEncryptorRegistry;
+import org.jasypt.util.password.PasswordEncryptor;
+import org.jasypt.util.password.StrongPasswordEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfigurator {
+
+    @Value("${jasypt.encryptor.password}")
+    private String password;
+    @Value("${jasypt.encryptor.algorythm}")
+    private String algorythm;
+
+    @Bean
+    public PasswordEncryptor defaultStringEncryptor(){
+
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setProvider(new BouncyCastleProvider());
+        encryptor.setAlgorithm(algorythm);
+        encryptor.setKeyObtentionIterations(1000);
+        encryptor.setPassword(password);
+        HibernatePBEEncryptorRegistry.getInstance()
+                .registerPBEStringEncryptor("defaultStringEncryptor", encryptor);
+        return new StrongPasswordEncryptor();
+    }
+}
