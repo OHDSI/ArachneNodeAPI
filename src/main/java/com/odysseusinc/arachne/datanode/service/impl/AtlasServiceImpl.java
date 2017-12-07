73d5e540d908063a5da384407f301309cef9015f
MMironov <mikhail.mironov@odysseusinc.com>
2017-12-07 16:05:37 +0300

    ARACHNE-1649 use login client

--- src/main/java/com/odysseusinc/arachne/datanode/service/impl/AtlasServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/datanode/service/impl/AtlasServiceImpl.java
@@ -32,11 +32,12 @@ import com.odysseusinc.arachne.datanode.exception.AuthException;
 import com.odysseusinc.arachne.datanode.service.AtlasService;
 import com.odysseusinc.arachne.datanode.service.client.atlas.AtlasAuthSchema;
 import com.odysseusinc.arachne.datanode.service.client.atlas.AtlasClient;
+import com.odysseusinc.arachne.datanode.service.client.atlas.AtlasClientConfig;
+import com.odysseusinc.arachne.datanode.service.client.atlas.AtlasLoginClient;
 import com.odysseusinc.arachne.system.settings.api.v1.dto.SystemSettingDTO;
 import com.odysseusinc.arachne.system.settings.api.v1.dto.SystemSettingsGroupDTO;
 import com.odysseusinc.arachne.system.settings.repository.SystemSettingsGroupRepository;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -50,9 +51,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
@@ -61,6 +60,14 @@ public class AtlasServiceImpl implements AtlasService {
     private final GenericConversionService conversionService;
     private final SystemSettingsGroupRepository systemSettingsGroupRepository;
     private HttpHeaders headers;
+
+    private static final String ATLAS_SETTINGS_GROUP_NAME = "atlas";
+    private static final String ATLAS_HOST = "atlas.host";
+    private static final String ATLAS_PORT = "atlas.port";
+    private static final String ATLAS_CONTEXT = "atlas.urlContext";
+    private static final String ATLAS_AUTH_SCHEMA = "atlas.auth.schema";
+    private static final String ATLAS_AUTH_USERNAME = "atlas.auth.username";
+    private static final String ATLAS_AUTH_PASSWORD = "atlas.auth.password";
 
     @Autowired
     public AtlasServiceImpl(GenericConversionService genericConversionService,
@@ -77,18 +84,17 @@ public class AtlasServiceImpl implements AtlasService {
     public String checkConnection() {
 
         SystemSettingsGroupDTO systemSettingsGroup =
-                conversionService.convert(systemSettingsGroupRepository.findByName("atlas"), SystemSettingsGroupDTO.class);
+                conversionService.convert(systemSettingsGroupRepository.findByName(ATLAS_SETTINGS_GROUP_NAME), SystemSettingsGroupDTO.class);
         Map<String, String> settings = systemSettingsGroup.getFieldList().stream().collect(Collectors.toMap(SystemSettingDTO::getName,
                 SystemSettingDTO::getValue));
-        String url = settings.get("atlas.host") + ":" + settings.get("atlas.port")
-                + (StringUtils.isBlank(settings.get("atlas.urlContext")) ? ""
-                : "/" + settings.get("atlas.urlContext"));
+        String url = AtlasClientConfig.getAtlasUrl(settings.get(ATLAS_HOST),
+                Integer.valueOf(settings.get(ATLAS_PORT)), settings.get(ATLAS_CONTEXT));
         headers = new HttpHeaders();
         headers.setContentType(MediaType.valueOf(MediaType.APPLICATION_FORM_URLENCODED_VALUE));
 
         String atlasVersion = checkVersion(url);
-        String atlasToken = authToAtlas(url, settings.get("atlas.auth.schema"),
-                settings.get("atlas.auth.username"), settings.get("atlas.auth.password"));
+        String atlasToken = authToAtlas(url, settings.get(ATLAS_AUTH_SCHEMA),
+                settings.get(ATLAS_AUTH_USERNAME), settings.get(ATLAS_AUTH_PASSWORD));
         headers.add(AtlasAuthRequestInterceptor.AUTHORIZATION_HEADER, AtlasAuthRequestInterceptor.BEARER_PREFIX + atlasToken);
 
         getCohortDefinitionCount(url);
@@ -113,21 +119,15 @@ public class AtlasServiceImpl implements AtlasService {
     private String authToAtlas(String url, String authSchema, String login, String password) {
 
         String atlasToken = null;
+        AtlasLoginClient atlasLoginClient = AtlasClientConfig.buildAtlasLoginClient(url);
         if (!StringUtils.isBlank(authSchema)) {
-            HttpEntity request = new HttpEntity<>("login=" +
-                    login + "&password=" + password, headers);
-            ResponseEntity<String> response;
             try {
                 switch (AtlasAuthSchema.valueOf(authSchema)) {
                     case DATABASE:
-                        response = atlasRestTemplate.postForEntity(
-                                new URI(url + Constants.Atlas.LOGIN_DB), request, String.class);
-                        atlasToken = response.getHeaders().get(AtlasAuthRequestInterceptor.BEARER_PREFIX.trim()).get(0);
+                        atlasToken = atlasLoginClient.loginDatabase(login, password);
                         break;
                     case LDAP:
-                        response = atlasRestTemplate.postForEntity(
-                                new URI(url + Constants.Atlas.LOGIN_LDAP), request, String.class);
-                        atlasToken = response.getHeaders().get(AtlasAuthRequestInterceptor.BEARER_PREFIX.trim()).get(0);
+                        atlasToken = atlasLoginClient.loginLdap(login, password);
                         break;
                     case NONE:
                         throw new AuthException("Atlas auth error");
