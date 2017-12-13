d3550d4c15b23709d05e8446ba3ed3559c35a34b
Alexandr Ryabokon <mr.alexandr.ryabokon@gmail.com>
2017-12-13 11:21:26 +0300

    ARACHNE-1720 Fixed test

--- src/test/java/com/odysseusinc/arachne/datanode/TestApplication.java
+++ src/test/java/com/odysseusinc/arachne/datanode/TestApplication.java
@@ -24,20 +24,15 @@ package com.odysseusinc.arachne.datanode;
 
 import com.odysseusinc.arachne.datanode.config.WebSecurityConfig;
 import javax.annotation.Resource;
-import javax.sql.DataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -69,11 +64,11 @@ public class TestApplication {
         SpringApplication.run(TestApplication.class, args);
     }
 
-    @Primary
+   /* @Primary
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource")
     public DataSource primaryDataSource() {
 
         return DataSourceBuilder.create().build();
-    }
+    }*/
 }
