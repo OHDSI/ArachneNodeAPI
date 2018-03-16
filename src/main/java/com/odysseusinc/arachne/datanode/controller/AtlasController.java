32ecd41b9459ea8967660d89711e2101aab755c4
Pavel Grafkin <pavgra@gmail.com>
2018-03-16 17:14:25 +0300

    ARACHNE-1995. Fixes after code-review #3

--- src/main/java/com/odysseusinc/arachne/datanode/controller/AtlasController.java
+++ src/main/java/com/odysseusinc/arachne/datanode/controller/AtlasController.java
@@ -1,3 +1,25 @@
+/*
+ *
+ * Copyright 2018 Observational Health Data Sciences and Informatics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Pavel Grafkin
+ * Created: March 15, 2018
+ *
+ */
+
 package com.odysseusinc.arachne.datanode.controller;
 
 import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
@@ -47,8 +69,8 @@ public class AtlasController {
     ) {
 
         Pageable search = new PageRequest(pageable.getPageNumber() - 1, pageable.getPageSize(), pageable.getSort());
-        Page<Atlas> atlasList = atlasService.findAll(search);
-        return atlasList.map(a -> conversionService.convert(a, AtlasDTO.class));
+        Page<Atlas> atlasPage = atlasService.findAll(search);
+        return atlasPage.map(a -> conversionService.convert(a, AtlasDTO.class));
     }
 
     @ApiOperation("Get Atlas detailed info")
