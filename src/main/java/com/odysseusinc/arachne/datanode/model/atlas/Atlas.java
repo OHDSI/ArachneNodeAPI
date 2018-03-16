e8b12558e817f3c5a6f79587cf5347dde6eedda4
Pavel Grafkin <pavgra@gmail.com>
2018-03-16 15:49:02 +0300

    ARACHNE-1995. Fixes after code-review #2

--- src/main/java/com/odysseusinc/arachne/datanode/model/atlas/Atlas.java
+++ src/main/java/com/odysseusinc/arachne/datanode/model/atlas/Atlas.java
@@ -1,6 +1,29 @@
+/*
+ *
+ * Copyright 2018 Observational Health Data Sciences and Informatics
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Pavel Grafkin
+ * Created: March 15, 2018
+ *
+ */
+
 package com.odysseusinc.arachne.datanode.model.atlas;
 
 import com.odysseusinc.arachne.datanode.service.client.atlas.AtlasAuthSchema;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -58,7 +81,7 @@ public class Atlas {
     @Override
     public int hashCode() {
 
-        return getId() != null ? getId().hashCode() : 0;
+        return Objects.hashCode(getId());
     }
 
     public Atlas() {
