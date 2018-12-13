035a2eed6788c3b9df428261390b518a4187cfeb
Maria Pozhidaeva <maria.pozhidaeva@odysseusinc.com>
2018-12-13 14:00:39 +0300

    ARACHNE-2586 - Changed network for achilles

--- src/main/java/com/odysseusinc/arachne/datanode/service/impl/AchillesServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/datanode/service/impl/AchillesServiceImpl.java
@@ -535,7 +535,7 @@ public class AchillesServiceImpl implements AchillesService {
                 )
                 .withVolumes(outputVolume)
                 .withBinds(new Bind(workDir.toString(), outputVolume))
-                .withNetworkMode("host")
+                .withNetworkMode("arachnenetwork")
                 .exec();
         LOGGER.debug("Container created: {}", container.getId());
         final boolean debugEnabled = LOGGER.isDebugEnabled();
