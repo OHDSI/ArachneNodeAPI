6293cca1da3fcf526fb55e908e87234568249f99
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-04-26 19:17:17 +0300

    removes redundant methods

--- src/main/java/com/odysseusinc/arachne/datanode/service/client/atlas/AtlasClient2_7.java
+++ src/main/java/com/odysseusinc/arachne/datanode/service/client/atlas/AtlasClient2_7.java
@@ -13,20 +13,12 @@ public interface AtlasClient2_7 extends AtlasClient {
 	@RequestLine("GET /prediction")
 	List<PredictionAnalysis> getPatientLevelPredictions();
 
-	@RequestLine("GET /prediction/{id}/download?packageName={package}")
-	@Headers("Accept: " + MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	byte[] getPrediction(@Param("id") Integer id, @Param("package") String packageName);
-
 	@RequestLine("GET /prediction/{id}/export")
 	@Headers("Accept: " + MediaType.APPLICATION_JSON_VALUE)
 	JsonNode getPrediction(@Param("id") Integer id);
 
 	@RequestLine("GET /estimation")
 	List<EstimationAnalysis> getEstimations();
-
-	@RequestLine("GET /estimation/{id}/download?packageName={package}")
-	@Headers("Accept: " + MediaType.APPLICATION_OCTET_STREAM_VALUE)
-	byte[] getEstimation(@Param("id") Integer id, @Param("package") String packageName);
 
 	@RequestLine("GET /estimation/{id}/export")
 	@Headers("Accepts: " + MediaType.APPLICATION_JSON_VALUE)
