aa38179d90299831c33b140fac5eb20ce5c6b9e4
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-07-09 18:01:46 +0300

    fixes migration

--- src/main/resources/db/migration/V20190708193049__schema-add-analysis-central-id.sql
+++ src/main/resources/db/migration/V20190708193049__schema-add-analysis-central-id.sql
@@ -1,3 +1,26 @@
+CREATE TABLE IF NOT EXISTS analyses
+(
+	id                        BIGSERIAL
+		PRIMARY KEY,
+	executable_filename       VARCHAR NOT NULL,
+	callback_password         VARCHAR NOT NULL,
+	update_status_callback    VARCHAR NOT NULL,
+	result_callback           VARCHAR NOT NULL,
+	data_source_id            BIGINT  NOT NULL
+		REFERENCES datasource
+		ON DELETE CASCADE,
+	analysis_folder           VARCHAR NOT NULL,
+	stdout                    VARCHAR,
+	result_status             VARCHAR,
+	title                     VARCHAR(255),
+	study_id                  BIGINT REFERENCES studies ON DELETE CASCADE,
+	author_email              VARCHAR(100),
+	author_first_name         VARCHAR(50),
+	author_last_name          VARCHAR(50),
+	type                      VARCHAR,
+	inner_executable_filename VARCHAR
+);
+
 ALTER TABLE analyses ADD IF NOT EXISTS central_id BIGINT;
 
 UPDATE analyses SET central_id = id WHERE analyses.central_id IS NULL;
@@ -5,11 +28,11 @@ UPDATE analyses SET central_id = id WHERE analyses.central_id IS NULL;
 CREATE SEQUENCE IF NOT EXISTS analyses_id_seq START WITH 5000;
 SELECT setval('analyses_id_seq', (SELECT MAX(id) FROM analyses) + 1);
 
-ALTER TABLE analysis_files DROP CONSTRAINT analysis_files_analysis_id_fkey;
+ALTER TABLE analysis_files DROP CONSTRAINT IF EXISTS analysis_files_analysis_id_fkey;
 
-ALTER TABLE analysis_code_files DROP CONSTRAINT analysis_code_files_analysis_id_fkey;
+ALTER TABLE analysis_code_files DROP CONSTRAINT IF EXISTS analysis_code_files_analysis_id_fkey;
 
-ALTER TABLE analysis_state_journal DROP CONSTRAINT analysis_state_journal_analysis_id_fkey;
+ALTER TABLE analysis_state_journal DROP CONSTRAINT IF EXISTS analysis_state_journal_analysis_id_fkey;
 
 UPDATE analyses SET id = nextval('analyses_id_seq');
 
