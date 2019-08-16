b8238890680b25ca008463d089773f7544fd4b1e
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-08-16 19:26:41 +0300

    cleanup obsolete code

--- src/main/java/com/odysseusinc/arachne/datanode/config/WebSecurityConfig.java
+++ src/main/java/com/odysseusinc/arachne/datanode/config/WebSecurityConfig.java
@@ -34,13 +34,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
@@ -53,13 +51,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserService userService;
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-
-        return super.authenticationManagerBean();
-    }
 
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
