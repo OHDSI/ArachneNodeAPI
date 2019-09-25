9b305d04fc39f2dab93607de8b80a930d0f0c0bf
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-09-25 18:44:29 +0300

    fixes BigQuery datasource create/edit

---
+++ src/main/java/com/odysseusinc/arachne/datanode/dto/datasource/validation/ValidCredentials.java
@@ -0,0 +1,44 @@
+/*
+ *
+ * Copyright 2019 Odysseus Data Services, inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Company: Odysseus Data Services, Inc.
+ * Product Owner/Architecture: Gregory Klebanov
+ * Authors: Pavel Grafkin, Vitaly Koulakov, Anastasiia Klochkova, Sergej Suvorov, Anton Stepanov
+ * Created: Sep 25, 2019
+ *
+ */
+
+package com.odysseusinc.arachne.datanode.dto.datasource.validation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Documented
+@Target( {ElementType.TYPE})
+@Retention( RetentionPolicy.RUNTIME )
+@Constraint(validatedBy = CreateDataSourceCredentialsValidator.class)
+public @interface ValidCredentials {
+
+    String message() default "{org.hibernate.validator.constraints.NotBlank.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
