f0b39323d56f1c17f9ce4c66bd80b5c1d310e613
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-10-16 18:21:58 +0300

    tests added

--- src/test/java/com/odysseusinc/arachne/datanode/config/CentralIntegrationServiceTestConfig.java
+++ src/test/java/com/odysseusinc/arachne/datanode/config/CentralIntegrationServiceTestConfig.java
@@ -20,25 +20,23 @@
  *
  */
 
-package com.odysseusinc.arachne.datanode.model.datanode.validation;
+package com.odysseusinc.arachne.datanode.config;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import javax.validation.Constraint;
-import javax.validation.Payload;
+import com.odysseusinc.arachne.datanode.service.CentralIntegrationService;
+import org.mockito.Mockito;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
-@Documented
-@Target({ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = { DataNodeTokenValidator.class })
-public @interface NonEmptyToken {
+@Profile("test")
+@Configuration
+public class CentralIntegrationServiceTestConfig {
 
-    String message() default "{org.hibernate.validator.constraints.NotBlank.message}";
+    @Primary
+    @Bean
+    public CentralIntegrationService centralIntegrationService() {
 
-    Class<?>[] groups() default {};
-
-    Class<? extends Payload>[] payload() default {};
+        return Mockito.mock(CentralIntegrationService.class);
+    }
 }
