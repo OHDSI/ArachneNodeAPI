b38681382ef6c1f36708a08568338f951ee904ec
Vitaly Koulakov <vitaly.koulakov@gmail.com>
2019-10-18 11:31:33 +0300

    fixes after review

--- src/main/java/com/odysseusinc/arachne/datanode/service/impl/DataSourceServiceImpl.java
+++ src/main/java/com/odysseusinc/arachne/datanode/service/impl/DataSourceServiceImpl.java
@@ -127,7 +127,7 @@ public class DataSourceServiceImpl implements DataSourceService {
     public void createOnCentral(User owner, DataSource dataSource) {
 
         AutoDetectedFields autoDetectedFields = autoDetectFields(dataSource);
-        if (Objects.equals(NETWORK, dataNodeService.getDataNodeMode())) {
+        if (NETWORK == dataNodeService.getDataNodeMode()) {
             CommonDataSourceDTO commonDataSourceDTO = conversionService.convert(dataSource, CommonDataSourceDTO.class);
             commonDataSourceDTO.setModelType(autoDetectedFields.getCommonModelType());
             commonDataSourceDTO.setCdmVersion(autoDetectedFields.getCdmVersion());
@@ -197,7 +197,7 @@ public class DataSourceServiceImpl implements DataSourceService {
 
         final String name = dataSource.getName();
         // Prevents name update in Standalone mode when datasource is synchronized
-        if (Objects.equals(STANDALONE, dataNodeService.getDataNodeMode()) && exists.getCentralId() != null && !Objects.equals(name, exists.getName())) {
+        if (STANDALONE == dataNodeService.getDataNodeMode() && exists.getCentralId() != null && !Objects.equals(name, exists.getName())) {
             throw new ValidationException("Cannot change Data source name which is published to Central in Standalone mode");
         }
         if (Objects.nonNull(name)) {
@@ -295,7 +295,7 @@ public class DataSourceServiceImpl implements DataSourceService {
             CommonDataSourceDTO commonDataSourceDTO = conversionService.convert(dataSource, CommonDataSourceDTO.class);
             commonDataSourceDTO.setModelType(autoDetectedFields.getCommonModelType());
             commonDataSourceDTO.setCdmVersion(autoDetectedFields.getCdmVersion());
-            if (Objects.equals(NETWORK, dataNodeService.getDataNodeMode())) {
+            if (NETWORK == dataNodeService.getDataNodeMode()) {
                 integrationService.sendDataSourceUpdateRequest(
                         user,
                         dataSource.getCentralId(),
