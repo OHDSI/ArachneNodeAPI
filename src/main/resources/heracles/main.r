06a21092ee78c243c91f195f977b84d8795b0508
alexandr cumarav <https://localhost:8080/study-manager/studies/6>
2019-11-19 10:26:30 +0100

    Heracles Analysis Support - delete unpacked files

--- src/main/resources/heracles/main.r
+++ src/main/resources/heracles/main.r
@@ -37,4 +37,6 @@ run_cohort_characterization(cohortDefinitionSqlPath = file.path(workDir, "{{init
                                                    "conditionsByIndex" = TRUE,
                                                    "proceduresByIndex" = TRUE,
                                                    "drugsByIndex" = TRUE)
-)
+
+
+unlink('cohortresults-sql', recursive = TRUE, force = TRUE)
