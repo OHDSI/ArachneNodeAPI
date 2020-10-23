7972b1395fdb70545be3c366d529935520961583
Alex Cumarav <7705583+acumarav@users.noreply.github.com>
2020-10-23 08:47:55 +0200

    Full Heracles analysis with dropdown details in zip parts (#437)

--- src/main/resources/heracles/main.r
+++ src/main/resources/heracles/main.r
@@ -10,35 +10,65 @@ pw <- Sys.getenv("DBMS_PASSWORD")
 cdmDatabaseSchema <- Sys.getenv("DBMS_SCHEMA")  # may be null if not defined in datasource's metadata
 resultsDatabaseSchema <- Sys.getenv("RESULT_SCHEMA")  # may be null if not defined in datasource's metadata
 
-run_cohort_characterization(cohortDefinitionSqlPath = file.path(workDir, "{{initialFileName}}"),
-                            outputFolder = file.path(workDir, "output"),
-                            dbms = dbms,
-                            connectionString = connStr,
-                            user = user,
-                            password = pw,
-                            cdmDatabaseSchema = cdmDatabaseSchema,
-                            resultsDatabaseSchema = resultsDatabaseSchema,
-                            cohortTable = "cohort",
-                            includeDrilldownReports = FALSE,
-                            includedReports = list("cohortSpecific" = TRUE,
-                                                   "death" = TRUE,
-                                                   "cohortObservationPeriod" = TRUE,
-                                                   "person" = FALSE,
-                                                   "dataCompleteness" = TRUE,
-                                                   "dashboard" = TRUE,
-                                                   "heraclesHeel" = TRUE,
-                                                   "entropy" = TRUE,
-                                                   "conditionTreemap" = TRUE,
-                                                   "drugEraTreemap" = TRUE,
-                                                   "drugExposuresTreemap" = TRUE,
-                                                   "procedureTreemap" = TRUE,
-                                                   "visitTreemap" = TRUE,
-                                                   "conditionEraTreemap" = TRUE,
-                                                   "conditionsByIndex" = TRUE,
-                                                   "proceduresByIndex" = TRUE,
-                                                   "drugsByIndex" = TRUE)
+library(DatabaseConnector)
+library(SqlRender)
 
 
+if ("impala" == dbms) {
+  driverPath <- Sys.getenv("IMPALA_DRIVER_PATH")
+  if (missing(driverPath) || is.null(driverPath) || driverPath == ''){
+    driverPath <- "/impala"
+  }
+  connectionDetails <- createConnectionDetails(dbms=dbms,
+                                               connectionString=connStr,
+                                               user=user,
+                                               password=pw,
+                                               pathToDriver=driverPath)
+} else {
+  connectionDetails <- createConnectionDetails(dbms=dbms,
+                                               connectionString=connStr,
+                                               user=user,
+                                               password=pw)
+}
+print(connectionDetails)
+
+start.time <- Sys.time()
+cohortDefinitionSqlPath <- file.path(workDir, "{{initialFileName}}")
+cohortId <- run_cohort_characterization(cohortDefinitionSqlPath = cohortDefinitionSqlPath,
+                                        connectionDetails,
+                                        cdmDatabaseSchema = cdmDatabaseSchema,
+                                        resultsDatabaseSchema = resultsDatabaseSchema,
+                                        cohortTable = "cohort"
 )
+
+exportResults(
+  cohortId = cohortId,
+  outputFolder = file.path(workDir, "output"),
+  connectionDetails = connectionDetails,
+  cdmDatabaseSchema = cdmDatabaseSchema,
+  resultsDatabaseSchema = resultsDatabaseSchema,
+  cohortTable = "cohort",
+  cohortDefinitionSqlPath = cohortDefinitionSqlPath,
+  includeDrilldownReports = T,
+  includedReports = list("cohortSpecific" = TRUE,
+                         "death" = TRUE,
+                         "cohortObservationPeriod" = TRUE,
+                         "person" = TRUE,
+                         "dataCompleteness" = TRUE,
+                         "dashboard" = TRUE,
+                         "heraclesHeel" = TRUE,
+                         "entropy" = TRUE,
+                         "conditionTreemap" = TRUE,
+                         "drugEraTreemap" = TRUE,
+                         "drugExposuresTreemap" = TRUE,
+                         "procedureTreemap" = TRUE,
+                         "visitTreemap" = TRUE,
+                         "conditionEraTreemap" = TRUE,
+                         "conditionsByIndex" = TRUE,
+                         "proceduresByIndex" = TRUE,
+                         "drugsByIndex" = TRUE)
+)
+
+print(paste("Total Execution Time ", Sys.time() -start.time, sep = " : "))
 
 unlink('cohortresults-sql', recursive = TRUE, force = TRUE)
