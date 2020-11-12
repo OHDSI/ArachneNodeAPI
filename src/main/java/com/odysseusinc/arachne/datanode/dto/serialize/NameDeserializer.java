cb304d16f274a99a96d89c813c594bd1db3c1a5a
alex cumarav <tsumarev@gmail.com>
2020-11-12 10:04:33 +0100

    adding tests

--- src/main/java/com/odysseusinc/arachne/datanode/dto/serialize/NameDeserializer.java
+++ src/main/java/com/odysseusinc/arachne/datanode/dto/serialize/NameDeserializer.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright 2018 Odysseus Data Services, inc.
+ * Copyright 2020 Odysseus Data Services, inc.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
