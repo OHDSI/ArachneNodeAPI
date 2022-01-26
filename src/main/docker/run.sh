6ad20f34159f9c67a9e6a3cf34dbf71bb7925832
Dmitry S <87665105+dmitrys-odysseus@users.noreply.github.com>
2022-01-26 15:24:31 +0100

    Enable memory dump - correct typo (#498)
    Co-authored-by: dmitrys <dmitrys-odysseus@github.com>

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 startservices.sh
 echo "DATANODE WILL START"
-java -XX:+HeadDumpOnOutOfMemoryError -XX:HeapDumpPath=/dump -Djava.security.egd=file:/dev/./urandom -jar datanode.jar
+java -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/dump -Djava.security.egd=file:/dev/./urandom -jar datanode.jar
 exit 0
