b843fa1267f68944fea0efdd3a18d893d8ca6a04
dmitrys <dmitrys-odysseus@github.com>
2022-05-26 09:33:34 +0300

    Update build number handling to prevent runtime failures and provide a default value
    (cherry picked from commit 0a3b35ca92a9cd0e4f3592213035c123c290cc73)

--- src/main/java/com/odysseusinc/arachne/datanode/controller/BuildNumberController.java
+++ src/main/java/com/odysseusinc/arachne/datanode/controller/BuildNumberController.java
@@ -25,8 +25,12 @@ package com.odysseusinc.arachne.datanode.controller;
 import com.odysseusinc.arachne.datanode.BuildNumberDTO;
 import com.odysseusinc.arachne.datanode.util.CentralUtil;
 import io.swagger.annotations.ApiOperation;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,19 +38,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BuildNumberController {
 
-    @Value("${build.number}")
-    private String buildNumber;
-    @Value("${build.id}")
-    private String buildId;
-    @Value("${project.version}")
-    private String projectVersion;
+    private static final String SOURCE = "META-INF/version.properties";
+    private final Logger log = LoggerFactory.getLogger(BuildNumberController.class);
 
-    private CentralUtil centralUtil;
+    private final String buildNumber;
+    private final String buildId;
+    private final String projectVersion;
+    private final String centralUrl;
 
     @Autowired
     public BuildNumberController(CentralUtil centralUtil) {
 
-        this.centralUtil = centralUtil;
+        InputStream is = getClass().getClassLoader().getResourceAsStream(SOURCE);
+        Properties properties = new Properties();
+        try {
+            properties.load(is);
+        } catch (IOException e) {
+            log.warn("Unable to read [{}], version info unavailable. Error details: {}.", SOURCE, e.getMessage());
+        }
+        projectVersion = properties.getProperty("version", "dev");;
+        buildNumber = properties.getProperty("buildNumber", "dev");
+        buildId = properties.getProperty("buildId", "dev");;
+        centralUrl = centralUtil.getCentralUrl();
+        log.info("Version [{}], build [{}] @ [{}]", projectVersion, buildNumber, buildId);
+        log.info("Central url [{}]", centralUrl);
     }
 
     @ApiOperation(value = "Get build number")
@@ -54,25 +69,14 @@ public class BuildNumberController {
     public BuildNumberDTO buildNumber() {
 
         BuildNumberDTO buildNumberDTO = new BuildNumberDTO();
-        buildNumberDTO.setBuildNumber(getBuildNumber());
-        buildNumberDTO.setBuildId(getBuildId());
-        buildNumberDTO.setProjectVersion(getProjectVersion());
-        buildNumberDTO.setCentralUrl(centralUtil.getCentralUrl());
+        buildNumberDTO.setBuildNumber(buildNumber);
+        buildNumberDTO.setBuildId(buildId);
+        buildNumberDTO.setProjectVersion(projectVersion);
+        buildNumberDTO.setCentralUrl(centralUrl);
         return buildNumberDTO;
     }
 
-    protected String getBuildNumber() {
-
-        return buildNumber;
-    }
-
-    protected String getBuildId() {
-
-        return buildId;
-    }
-
-    protected String getProjectVersion() {
-
+    public String getProjectVersion() {
         return projectVersion;
     }
 }
